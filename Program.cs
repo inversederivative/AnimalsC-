849fe73a51e05e4c979e896025d277751538a0ee
John Hatton <hattonjd@iu.edu>
2024-03-04 12:27:56 +0100

    Add Project Files

---
+++ Program.cs
@@ -0,0 +1,49 @@
+﻿namespace Animals
+{
+    abstract class Animal
+    {
+        public abstract void MakeSound();
+    }
+
+    class Dog : Animal
+    {
+        public override void MakeSound()
+        {
+            Console.WriteLine("Bark!");
+        }
+    }
+
+    class Cat : Animal
+    {
+        public override void MakeSound()
+        {
+            Console.WriteLine("Meow!");
+        }
+    }
+
+    class Horse : Animal
+    {
+        public override void MakeSound()
+        {
+            Console.WriteLine("Neihhh!");
+        }
+    }
+
+
+    class Program
+    {
+        public static int Main(string[] args)
+        {
+            Animal animal = new Dog();
+            animal.MakeSound();
+
+            animal = new Cat();
+            animal.MakeSound();
+
+            animal = new Horse();
+            animal.MakeSound(); 
+
+            return 0;
+        }
+    }
+}
